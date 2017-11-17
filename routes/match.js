689ab944491fa4bad7dd9f95348c23bc0820d756
SartineCat® <jobryant@163.com>
2017-11-17 16:52:44 +0800

    add match router

--- routes/match.js
+++ routes/match.js
@@ -1,9 +1,9 @@
+var express = require('express');
+var router = express.Router();
 
-/*
- * GET match API.
- */
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.send('respond in match');
+});
 
-exports.match = function(req, res){
-    res.send('matching');
-};
-
+module.exports = router;
