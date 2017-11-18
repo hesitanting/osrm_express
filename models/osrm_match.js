98f4480aa2dc0aaea0b2a2d27f3bd75db9fc38c9
SartineCat® <jobryant@163.com>
2017-11-18 16:21:31 +0800

    add:osrm_match model

---
+++ models/osrm_match.js
@@ -0,0 +1,17 @@
+var OSRM = require("../node_modules/osrm/lib/index");
+var osrm = new OSRM("./data/portugal-latest.osrm");
+
+
+
+function matching(options,callback) {
+    osrm.match(options, function(err, response) {
+        if (err) throw err;
+        callback(err,response);
+        // console.log(response.tracepoints); // array of Waypoint objects
+        // console.log(response.matchings); // array of Route objects
+    });
+}
+
+module.exports = {
+	matching:matching
+};
--- routes/match.js
+++ routes/match.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var match = require('../models/osrm_match');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.send('respond in match');
+});
+
+router.post('/',function (req,res,next) {
+
+    options={};
+    options['coordinates'] = req.param('coordinates');
+    options['timestamps'] = req.param('timestamps');
+
+    match.matching(options,function (match_err,match_res) {
+        result = {
+            err:match_err,
+            data:match_res
+        };
+        res.send(result);
+    })
 });
 
 module.exports = router;
