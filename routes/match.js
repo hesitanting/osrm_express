79f2defbace693a712e5c7237751036ba6d70956
SartineCat® <jobryant@163.com>
2017-11-23 14:07:39 +0800

    update: post router

--- routes/match.js
+++ routes/match.js
@@ -3,50 +3,57 @@ var router = express.Router();
 var match = require('../models/osrm_match');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-    options= req.query;
-    for (let param in options) {
-        try {
-            options[param] = JSON.parse(options[param])
-        } catch (e) {
-            continue;
-        }
-    }
-    try{
-        match.matching(options,function (match_err,match_res) {
-            result = {
-                err:match_err,
-                data:match_res
-            };
-            res.send(result);
-        })
-    }catch (e){
+router.get('/', function (req, res, next) {
+    options = req.query;
+    on_matching(res, options);
+});
 
+router.post('/', function (req, res, next) {
+    options = undefined;
+    if (req.body.coordinates) {
+        options = req.body
+    } else {
+        let body = '', jsonStr;
+        req.on('data', function (chunk) {
+            body += chunk;
+        });
+        req.on('end', function () {
+            try {
+                jsonStr = JSON.parse(body);
+                options = jsonStr;
+                on_matching(res, options)
+
+            } catch (err) {
+                res_content = {
+                    err: 'json parsing error: unknown post data'
+                };
+                res.send(res_content)
+            }
+        })
     }
 });
 
-router.post('/',function (req,res,next) {
-
-    options= req.body;
+function on_matching(res, options) {
     for (let param in options) {
         try {
-            options[param] = JSON.parse(options[param])
+            options[param] = JSON.parse(options[param]);
         } catch (e) {
-            continue;
         }
     }
-    try{
-        match.matching(options,function (match_err,match_res) {
+    try {
+        match.matching(options, function (match_err, match_res) {
             result = {
-                err:match_err,
-                data:match_res
+                err: match_err,
+                data: match_res
             };
             res.send(result);
         })
-    }catch (e){
-
+    } catch (err) {
+        res_content = {
+            err: err
+        };
+        res.send(res_content);
     }
-
-});
+}
 
 module.exports = router;
