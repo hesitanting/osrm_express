2202f78c2000aef37c76b380861183b10fce0bed
SartineCat® <jobryant@163.com>
2017-11-23 10:10:36 +0800

    add: add get for osrm_match

--- routes/match.js
+++ routes/match.js
@@ -4,11 +4,6 @@ var match = require('../models/osrm_match');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond in match');
-});
-
-router.post('/',function (req,res,next) {
-
     options= req.query;
     for (let param in options) {
         try {
@@ -17,14 +12,41 @@ router.post('/',function (req,res,next) {
             continue;
         }
     }
+    try{
+        match.matching(options,function (match_err,match_res) {
+            result = {
+                err:match_err,
+                data:match_res
+            };
+            res.send(result);
+        })
+    }catch (e){
 
-    match.matching(options,function (match_err,match_res) {
-        result = {
-            err:match_err,
-            data:match_res
-        };
-        res.send(result);
-    })
+    }
+});
+
+router.post('/',function (req,res,next) {
+
+    options= req.body;
+    for (let param in options) {
+        try {
+            options[param] = JSON.parse(options[param])
+        } catch (e) {
+            continue;
+        }
+    }
+    try{
+        match.matching(options,function (match_err,match_res) {
+            result = {
+                err:match_err,
+                data:match_res
+            };
+            res.send(result);
+        })
+    }catch (e){
+
+    }
+
 });
 
 module.exports = router;
