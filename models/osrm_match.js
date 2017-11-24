91a4e895cb7be829686fdd924139a80411071098
SartineCat® <jobryant@163.com>
2017-11-24 23:55:43 +0800

    modify: cancel throw err

--- models/osrm_match.js
+++ models/osrm_match.js
@@ -5,7 +5,7 @@ var osrm = new OSRM("../data/portugal-latest.osrm");
 
 function matching(options,callback) {
     osrm.match(options, function(err, response) {
-        if (err) throw err;
+        // if (err) throw err;
         callback(err,response);
         // console.log(response.tracepoints); // array of Waypoint objects
         // console.log(response.matchings); // array of Route objects
