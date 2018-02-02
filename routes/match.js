fb4a015fac1a54877a15983b53f7c0d6c9655fd8
SartineCat® <jobryant@163.com>
2018-02-02 15:20:07 +0800

    allow cross-domain

--- routes/match.js
+++ routes/match.js
@@ -3,6 +3,16 @@ var router = express.Router();
 var match = require('../models/osrm_match');
 
 /* GET users listing. */
+router.all('*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
+/* GET users listing. */
 router.get('/', function (req, res, next) {
     options = req.query;
     on_matching(res, options);
